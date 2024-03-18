a0f7745f3f877e54ab5e63b40f323b609d5e1e57
Chaemin Yooun <93826302+oyune@users.noreply.github.com>
2024-03-18 22:12:09 +0900

    Add dto
    - CancelReservationRequestDto
    - CheckReservationRequestDto
    - CreateLectureRequestDto
    - CreateLectureResponseDto
    - CreateReservationRequestDto
    - ReservationResponseDto

---
+++ class-management-system/src/main/java/com/example/classmanagementsystem/dto/CheckReservationRequestDto.java
@@ -0,0 +1,14 @@
+package com.example.classmanagementsystem.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CheckReservationRequestDto {
+    private Long reserverId;
+}
